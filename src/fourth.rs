f9f7ac65471993f2b8b5a202088fd29956a5f7b0
Quentin Santos <qsantos@qsantos.fr>
2023-03-02 14:31:34 +0100

    Implement *_back

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,4 +1,4 @@
-use std::cell::{Ref, RefCell};
+use std::cell::{Ref, RefCell, RefMut};
 use std::rc::Rc;
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
@@ -73,6 +73,60 @@ impl<T> List<T> {
             .as_ref()
             .map(|node| Ref::map(node.borrow(), |node| &node.value))
     }
+
+    pub fn peek_front_mut(&self) -> Option<RefMut<T>> {
+        self.first
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.value))
+    }
+
+    pub fn push_back(&mut self, value: T) {
+        let new_node = Node::new(value);
+        match self.last.take() {
+            None => {
+                self.last = Some(new_node.clone());
+                self.first = Some(new_node);
+            }
+            Some(prev) => {
+                prev.borrow_mut().next = Some(new_node.clone());
+                new_node.borrow_mut().prev = Some(prev);
+                self.last = Some(new_node);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        match self.last.take() {
+            None => None,
+            Some(node) => {
+                assert!(node.borrow().next.is_none());
+                // detach node
+                match node.borrow_mut().prev.take() {
+                    None => {
+                        self.first = None;
+                    }
+                    Some(prev) => {
+                        prev.borrow_mut().next = None;
+                        self.last = Some(prev);
+                    }
+                }
+                // unwrap the value
+                Some(Rc::try_unwrap(node).ok().unwrap().into_inner().value)
+            }
+        }
+    }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.last
+            .as_ref()
+            .map(|node| Ref::map(node.borrow(), |node| &node.value))
+    }
+
+    pub fn peek_back_mut(&self) -> Option<RefMut<T>> {
+        self.last
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.value))
+    }
 }
 
 impl<T> Default for List<T> {
@@ -112,16 +166,49 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // ---- back -----
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
 
     #[test]
     fn peek() {
         let mut list = List::new();
         assert!(list.peek_front().is_none());
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_front_mut().is_none());
+        assert!(list.peek_back_mut().is_none());
+
         list.push_front(1);
         list.push_front(2);
         list.push_front(3);
 
         assert_eq!(&*list.peek_front().unwrap(), &3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
     }
 }
